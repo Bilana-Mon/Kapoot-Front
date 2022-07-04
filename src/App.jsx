8046535fb1768363e49e5a4b8353149c528a1098
Bilana-Mon <bilanamoon@gmail.com>
2022-07-04 20:00:31 +0300

    added form logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
 
   return (
     <div className='bg-cyan-300'>
       <Link to="/signup">Signup</Link>
+      <div className='bg-cyan-300'>Welcome, user!</div>
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Signup() {
 
     const [inputs, setInputs] = useState({});
+    let navigate = useNavigate();
 
     const handleChange = (event) => {
         const name = event.target.name;
@@ -11,16 +13,27 @@ function Signup() {
         setInputs(values => ({ ...values, [name]: value }))
     };
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const form = {
+            nickname: formData.get('nickname'),
+            email: formData.get('email'),
+            password: formData.get('password')
+        }
+        const { data } = await axios.post("http://localhost:3000/signup", form); // connection to backend here
+        if (data.status !== parseInt('401')) {
+            navigate('/')
+        }
     };
 
 
+
     return (
-        <section className="flex flex-col px-8 py-3">
-            <div className="text-center"><h1 className="text-lg font-bold">Create an account</h1></div>
-            <div className="mx-auto w-96 h-screen flex flex-col rounded-md shadow-md mt-5 align-middle">
-                <div className="text-center text-md font-bold">Sign up with your email</div>
+        <section className="bg-gray-200 flex flex-col px-8 py-3 h-screen">
+            {/* <div className="text-center"><h1 className="text-lg font-bold">Create an account</h1></div> */}
+            <div className="bg-white mx-auto w-96 h-96 flex flex-col rounded-md shadow-md mt-5 align-middle">
+                <div className="text-center text-md font-bold mt-3">Sign up with your email</div>
                 <div>
                     <form onSubmit={handleSubmit} className="flex flex-col py-5 px-4">
                         <label htmlFor="nickname" className="font-bold text-sm">Nickname</label>
@@ -29,9 +42,10 @@ function Signup() {
                         <input className="p-2 w-full border solid border-1 border-gray-300 rounded mb-2" type="email" name='email' value={inputs.email || ""} onChange={handleChange} />
                         <label htmlFor="password" className="font-bold text-sm">Password</label>
                         <input className="p-2 w-full border solid border-1 border-gray-300 rounded mb-5" type="password" name='password' value={inputs.password || ""} onChange={handleChange} />
-                        <button className="border-2 border-solid border-gray-200 rounded p-2 font-bold hover:border-gray-300" type='submit'>Sign up</button>
+                        <button className="border-2 border-solid border-gray-200 rounded p-2 font-bold hover:border-gray-300 hover:bg-gray-200" type='submit'>Sign up</button>
                     </form>
                 </div>
+                <div className="px-4"><span>Already have an account?</span><span className="ml-1 text-blue-600 hover:underline"><a href="#">Log in</a></span></div>
             </div>
         </section>
     )
