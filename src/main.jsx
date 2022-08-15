f8aa86907eac417dc4146ec12ab925f537e017c7
Bilana-Mon <bilanamoon@gmail.com>
2022-08-15 14:15:31 +0300

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
-import PrivateRoutes from './utils/PrivateRoutes';
+import PrivateRoutes from './cmps/PrivateRoutes';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
