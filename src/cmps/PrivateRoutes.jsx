78465cbedad2bdca635dc56423acf8785fb0601c
Bilana-Mon <bilanamoon@gmail.com>
2022-11-08 19:42:41 +0200

    connected me route to privateRoute

--- src/cmps/PrivateRoutes.jsx
+++ src/cmps/PrivateRoutes.jsx
@@ -1,12 +1,28 @@
-import { Outlet, Navigate } from "react-router-dom";
-import { useSession } from "../hooks/useSession";
+import { useState, useEffect } from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+import { useSession } from '../hooks/useSession'
 
 const PrivateRoutes = () => {
-    const { isLogged } = useSession();
+    const [isFetchDone, setIsFetchDone] = useState(false)
+    const { isLogged } = useSession()
 
-    return (
-        isLogged ? <Outlet /> : <Navigate to='/login' />
-    )
+    useEffect(() => {
+        fetch('http://localhost:4000/auth/me', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((error) => {
+                console.log('error', error)
+            })
+    }, [])
+
+    return isLogged ? <Outlet /> : <Navigate to="/login" />
 }
 
-export default PrivateRoutes;
+export default PrivateRoutes
