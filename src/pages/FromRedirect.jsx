f191654c66a4dec1c36f122bb877e168a9e78834
Bilana-Mon <bilanamoon@gmail.com>
2022-11-21 16:30:00 +0200

    fixed url

--- src/pages/FromRedirect.jsx
+++ src/pages/FromRedirect.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { useSession } from '../hooks/useSession';
 import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export const FromRedirect = () => {
     let [searchParams, setSearchParams] = useSearchParams();
-
+    const [refresh, setRefresh] = useState(false);
     const navigate = useNavigate();
 
     const { setToken } = useSession();
 
     const userToken = searchParams.get('userToken');
-    const refreshPage = (url) => {
-        window.location.reload(true);
-        navigate(url);
-    };
 
     React.useEffect(() => {
         if (userToken) {
             setToken(userToken);
-            refreshPage(import.meta.env.VITE_APP_URL);
-            // navigate('/');
+            setRefresh(true);
+
+            navigate(import.meta.env.VITE_APP_URL);
         }
     }, []);
 
