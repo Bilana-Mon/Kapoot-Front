6c713c0bbc4a48fd0714aab262fa6ff1f23cd925
Bilana-Mon <bilanamoon@gmail.com>
2022-11-24 15:58:21 +0200

    fixed socket

--- src/pages/Questionnaire.jsx
+++ src/pages/Questionnaire.jsx
@@ -9,7 +9,7 @@ import { ReactComponent as ArrowBackIcon } from '../assets/icons/arrow-back.svg'
 const ENDPOINT = `${import.meta.env.VITE_APP_API_URL}`;
 console.log('socket', ENDPOINT);
 
-const socket = io();
+const socket = io(ENDPOINT);
 
 function Questionnaire() {
     const [loading, setLoading] = useState(true);
